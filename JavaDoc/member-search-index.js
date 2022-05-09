b4ee125a6ec308e2d1f6d639157d2f591cfc592e
Pyong99 <55272102+Pyong99@users.noreply.github.com>
2022-05-09 16:32:58 +0800

    Add files via upload

---
+++ JavaDoc/member-search-index.js
@@ -0,0 +1,12 @@
+memberSearchIndex = [{"p": "my.uum", "c": "App", "l": "App()", "u": "%3Cinit%3E()"}, {
+    "p": "my.uum",
+    "c": "App",
+    "l": "main(String[])",
+    "u": "main(java.lang.String[])"
+}, {"p": "my.uum", "c": "Thread1", "l": "run()"}, {"p": "my.uum", "c": "Thread2", "l": "run()"}, {
+    "p": "my.uum",
+    "c": "Thread1",
+    "l": "Thread1()",
+    "u": "%3Cinit%3E()"
+}, {"p": "my.uum", "c": "Thread2", "l": "Thread2()", "u": "%3Cinit%3E()"}];
+updateSearchResults();
---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,5 @@
+typeSearchIndex = [{"l": "All Classes and Interfaces", "u": "allclasses-index.html"}, {
+    "p": "my.uum",
+    "l": "App"
+}, {"p": "my.uum", "l": "Thread1"}, {"p": "my.uum", "l": "Thread2"}];
+updateSearchResults();
